0e8520efd2e8af2fa90277fae7ab1641e0cd1520
MartinaPerozzi <perozzimartina@gmail.com>
2023-03-30 18:01:54 +0200

    try to see

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
       <img class="my-1" src="{{ Vite::asset('resources/img/logo.png') }}" alt="">
       <h1 class="my-1">
         Laravel 9
-        <span class="fs-6 fw-light text-muted"> - Bootstrap Template</span>
+        <span class="fs-6 fw-light text-muted"> - Bootstrap Template </span>
       </h1>
 
     </div>
